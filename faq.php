9b87e32b4f32b62313ada7367ad25fde528cdf2f
moirmoto <kanten520212@gmail.com>
2022-09-05 15:31:46 +0900

    add faq page
    add faq page.

---
+++ faq.php
@@ -0,0 +1,16 @@
+<!-- トップページ -->
+<?php
+require_once 'header.php';
+require_once 'footer.php';
+?>
+
+<?php
+/* header.php 読み込み */
+head('faq', 'よくあるご質問');
+?>
+
+<!-- ここへFAQページのコーディングを記述してください -->
+<p>sample text</p>
+
+<!-- フッター -->
+<?php footer('index'); ?>
