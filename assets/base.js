4783bfe5d5f97365d08c4fa9fcccfb1d25506c0e
wiyco <72733890+wiyco@users.noreply.github.com>
2022-10-01 16:21:40 +0900

    Fix header issue when scroll to top in iOS
    - Fix header issue when scroll to top in iOS

--- assets/base.js
+++ assets/base.js
@@ -3,7 +3,7 @@ var winScrollTop = 0;
 
 $(window).on('scroll', function(){
     winScrollTop = $(this).scrollTop();
-    if(winScrollTop >= startPos){
+    if(winScrollTop >= startPos && winScrollTop >= 100){
         $('nav').addClass('hide');
     } else {
         $('nav').removeClass('hide');
