737f6b9937f45d6795ef367dad3aec1977e80db6
shun <shunshunshun112311231123@gmail.com>
2022-10-05 16:06:24 +0900

    ver_4
    いい感じですかね

--- contact.php
+++ contact.php
@@ -14,15 +14,10 @@ head('contact', 'お問い合わせ | IPUT学生団体');
 <main "contents">
 
     <!-- top -->
-    <div class="ONE" id="contact-top">
+    <div class="position-relative" id="contact-top">
         <img src="src/contact-top-img.jpg" class="w-100">
-<<<<<<< Updated upstream
-        <div class="TWO">
-            <div class="THREE">
-=======
         <div class="position-absolute top-50 start-50 translate-middle">
             <div class="text-center text-white">
->>>>>>> Stashed changes
                 <!-- お問い合わせ -->
                 <h1>お問い合わせ</h1>
                 <!-- CONTACT -->
@@ -34,37 +29,6 @@ head('contact', 'お問い合わせ | IPUT学生団体');
 
 
     <!-- main -->
-<<<<<<< Updated upstream
-    <div class="FOUR">
-        <p>当サイト又は、IPUT学生団体に関するお問い合わせは、下記のお問い合わせフォームよりご連絡ください。<br>
-            お問い合わせをする前に、よくある質問（FAQ）に同じ質問がないかご確認ください。<br>
-            尚、当サイト又は、IPUT学生団体以外のお問い合わせにつきましてはお答えすることが出来ませんのであらかじめご了承ください。
-        </p>
-        
-        <!-- Bootstrap よくある質問はこちらから -->
-        <div class="SIX_QUE">
-            <button type="button" class="btn btn-secondary btn-lg">よくある質問はこちらから</button>
-        </div>
-        <!-- 必須 お名前 -->
-        <div class="required_box">
-
-            <h5><span class="badge bg-secondary">必須</span></h5>
-            <div class="FIVE">
-                <h3><b>お名前</b></h3>
-            </div>
-        </div>
-        <!-- from input text large -->
-        <div class="input-group input-group-lg">
-            <span class="input-group-text" id="inputGroup-sizing-lg">お名前を入力してください</span>
-            <input type="text" class="form-control" aria-label="Sizing example input" aria-describedby="inputGroup-sizing-lg">
-        </div>
-        <!-- 必須 メールアドレス -->
-        <div class="SEVEN_mail">
-            <div class="required_box">
-                <h5><span class="badge bg-secondary">必須</span></h5>
-                <div class="FIVE">
-                    <h3><b>メールアドレス</b></h3>
-=======
     <div class="contact_main">
         <div class="description_p">
             <p>当サイト又は、IPUT学生団体に関するお問い合わせは、下記のお問い合わせフォームよりご連絡ください。<br>
@@ -105,36 +69,10 @@ head('contact', 'お問い合わせ | IPUT学生団体');
                 </div>
                 <div class="input-group input-group-lg">
                     <input type="text" class="form-control" placeholder="メールアドレスを入力してください" aria-label="Sizing example input" aria-describedby="inputGroup-sizing-lg">
->>>>>>> Stashed changes
                 </div>
             </div>
         </div>
         <!-- from input text large -->
-<<<<<<< Updated upstream
-        <div class="input-group input-group-lg">
-            <span class="input-group-text" id="inputGroup-sizing-lg">メールアドレスを入力してください</span>
-            <input type="text" class="form-control" aria-label="Sizing example input" aria-describedby="inputGroup-sizing-lg">
-        </div>
-        <!-- 必須 お問い合わせ内容 -->
-        <div class="required_box">
-            <h5><span class="badge bg-secondary">必須</span></h5>
-            <div class="FIVE">
-                <h3><b>お問い合わせ内容</b></h3>
-            </div>
-        </div>
-        <!-- from input Textarea -->
-        <div class="form-floating">
-            <textarea class="form-control" placeholder="Leave a comment here" id="floatingTextarea2" style="height: 100px"></textarea>
-            <label for="floatingTextarea2">Comments</label>
-        </div>
-        <!-- 左にトップページに戻る(Bootstrap Button　Large Secondary) 右送信する -->
-        <div class="top_button">
-            <button type="button" class="btn btn-secondary btn-lg">トップページへ戻る</button>
-        </div>
-        <div class="send_button">
-            <button type="button" class="btn btn-primary btn-lg">送信する</button>
-        </div>
-=======
 
         <!-- 必須 お問い合わせ内容 -->
         <div class="contact_sum">
@@ -160,15 +98,9 @@ head('contact', 'お問い合わせ | IPUT学生団体');
 
         </div>
 </main>
->>>>>>> Stashed changes
 
 
 
 
-<<<<<<< Updated upstream
-        <!-- フッター -->
-        <?php footer('index'); ?>
-=======
 <!-- フッター -->
 <?php footer('index'); ?>
->>>>>>> Stashed changes
