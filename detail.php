35055ea3db38d65f03b28d2c83df4f8e3e1e6f20
KoyamaShogo <koyama0729@outlook.jp>
2022-10-13 16:36:42 +0900

    add sub text

--- detail.php
+++ detail.php
@@ -12,5 +12,26 @@ head('detail', 'サークル詳細ページ | IPUT学生団体');
     <p class="top-text">Nectgramsプログラミングサークル</p>
 </div>
 
+<div class="top-sub-text">
+    <ul>
+        <li class="line">
+            <h2>所属人数</h2>
+            <p>40</p>
+        </li>
+        <li class="line">
+            <h2>活動日程</h2>
+            <p>毎週土曜日</p>
+        </li>
+        <li class="line">
+            <h2>活動場所</h2>
+            <p>コクーンタワー</p>
+        </li>
+        <li>
+            <h2>カテゴリ</h2>
+            <p>創造</p>
+        </li>
+    </ul>
+</div>
+
 
 <?php footer('detail'); ?>
