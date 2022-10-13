b1ee70cf11aa035b0c168230187fc817d510ed9e
KoyamaShogo <koyama0729@outlook.jp>
2022-10-13 16:32:24 +0900

    top image

---
+++ detail.php
@@ -0,0 +1,16 @@
+<?php
+require_once 'header.php';
+require_once 'footer.php';
+?>
+
+<?php
+head('detail', 'サークル詳細ページ | IPUT学生団体');
+?>
+
+<div class="top-image">
+    <img src="src/detail-top.png" />
+    <p class="top-text">Nectgramsプログラミングサークル</p>
+</div>
+
+
+<?php footer('detail'); ?>
