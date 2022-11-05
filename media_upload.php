46dd7826af606df7aa052a10f1cce8313ba506d6
moirmoto <kanten520212@gmail.com>
2022-11-05 14:58:39 +0900

    update circle post page

--- media_upload.php
+++ media_upload.php
@@ -1,35 +1,32 @@
 <?php
 /* Template Name: メディアアップロード */
+// サークル作成ページや、活動記録投稿ページの本文に画像を張り付ける際に実行される処理
+// 参考：https://wpdocs.osdn.jp/関数リファレンス/media_handle_upload
 
-// 参考にしたサイト：https://wpdocs.osdn.jp/関数リファレンス/media_handle_upload
-
-// nonce が有効で、ユーザーがこの投稿を編集可能であるかチェック。
+// WordPress Nonceチェック
 if ( 
-	isset( $_POST['my_image_upload_nonce'], $_POST['post_id'] ) 
+	isset( $_POST['my_image_upload_nonce'], $_POST['post_id'] )
 	&& wp_verify_nonce( $_POST['my_image_upload_nonce'], 'my_image_upload' )
 	&& current_user_can( 'edit_post', $_POST['post_id'] )
 ) {
-	// nonce が有効で、ユーザーが権限を持つので、続けて大丈夫。
-
-	// 下記のファイルに依存するのでフロントエンドではインクルードする必要がある。
 	require_once( ABSPATH . 'wp-admin/includes/image.php' );
 	require_once( ABSPATH . 'wp-admin/includes/file.php' );
 	require_once( ABSPATH . 'wp-admin/includes/media.php' );
 	
-	// WordPress にアップロードを処理させる。
-	// 注意: 'my_image_upload' は上のフォームで file input 要素の name 属性。
+	// WordPress にアップロードを処理させる
+	// 注意: 'my_image_upload' は上のフォームで file input 要素の name 属性
 	$attachment_id = media_handle_upload( 'my_image_upload', $_POST['post_id'] );
 	
 	if ( is_wp_error( $attachment_id ) ) {
-		// 画像のアップロード中にエラーが起きた。
+		// アップロード中にエラー
         echo $attachment_id.get_error_code();
 	} else {
-		// 画像のアップロードに成功 !
+		// アップロードに成功 
         echo wp_get_attachment_image_src($attachment_id, 'full')[0];
 	}
 
 } else {
-    // セキュリティチェック失敗、例えばユーザーにエラーを提示。
+    // セキュリティチェック失敗。
     echo "<h1>Access Denied!</h1>";
 }
 ?>
