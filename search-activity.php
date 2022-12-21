88c9e85b619e3d9f5cdcd781580624a59fb9c741
YumaMorimoto <kanten520212@gmail.com>
2022-12-21 21:02:20 +0900

    add condition specification in search-activity.php

--- search-activity.php
+++ search-activity.php
@@ -21,9 +21,28 @@ $args = array(
     'posts_per_page' => 12,            // 投稿取得数
     'paged'          => $paged,        // 現在のページ
 );
+
+// 条件指定があった場合
+$param_circle  = get_params('c');  // サークル名（カテゴリID）
+$param_event_1 = get_params('e1'); // 行事・イベント
+$param_event_2 = get_params('e2'); // 活動記録
+$param_event_3 = get_params('e3'); // 重要連絡
+
+if ( $param_circle )  $args['cat'] = $param_circle;
+$tags = array();
+if ( $param_event_1 ) $tags[] = get_tags(array('slug' => '行事・イベント'))[0]->term_id;
+if ( $param_event_2 ) $tags[] = get_tags(array('slug' => '活動記録'))[0]->term_id;
+if ( $param_event_3 ) $tags[] = get_tags(array('slug' => '重要連絡'))[0]->term_id;
+if ( !empty($tags) ) {
+    $args['tag__and'] = $tags;
+}
+
 $the_query = new WP_Query( $args );    // 投稿データ
 
 get_header();
+
+var_dump(get_tags(array('slug' => '行事・イベント')));
+//var_dump($args);
 ?>
 
 <div class="top">
@@ -38,16 +57,29 @@ get_header();
         <div class="search-settings">
             <!-- サークルを選択する -->
             <div class="dropdown">
-                <button class="btn btn-secondary dropdown-toggle" type="button"
-                id="dropdownMenuButton1" data-bs-toggle="dropdown" aria-expanded="false">
-                    サークルを選択する
-                </button>
-                <ul class="dropdown-menu" aria-labelledby="dropdownMenuButton1">
-                    <li><a class="dropdown-item" href="#">サークル名1</a></li>
-                    <li><a class="dropdown-item" href="#">サークル名2</a></li>
-                    <li><a class="dropdown-item" href="#">サークル名3</a></li>
-                </ul>
+                <select class="form-select form-select-sm" name="organizationId" style="flex:1" aria-label="所属サークルを選択する">
+                    <!-- set value post id -->
+                    <option value="false" selected>サークルを選択する</option>
+                    <?php
+                    // ログイン中のユーザーのサークル一覧
+                    $args = array(
+                        'posts_per_page' => -1,
+                        'post_type' => 'circle',
+                        'post_status' => 'publish'
+                    );
+                    $posts = get_posts( $args );
+                    
+                    foreach( $posts as $post ):
+                        $category_name = get_the_category( $post->ID )[0]->cat_name;
+                        $category_id = get_the_category( $post->ID )[0]->cat_ID;
+                    ?>
+                    <option value="<?php echo $category_id ?>"><?php echo $category_name ?></option>
+                    <?php
+                    endforeach;
+                    ?>
+                </select>
             </div>
+
             <input type="checkbox" class="btn-check" id="btn-check1" autocomplete="off">
             <label class="tag btn btn-outline-secondary" for="btn-check1">行事・イベント</label>
             <input type="checkbox" class="btn-check" id="btn-check2" autocomplete="off">
@@ -75,7 +107,9 @@ get_header();
                 <div class="row g-0">
                     <div class="thumbnail col-4 col-lg-12">
                         <?php
+                        // カスタムデータ取得
                         $post_custom = get_post_custom( get_the_ID() );
+                        // サムネイルURL
                         $thumbnail_url = !empty($post_custom['topImage'][0]) ? wp_get_attachment_image_src( $post_custom['topImage'][0] )[0] : get_theme_file_uri('src/no_image_activity.png');
                         ?>
                         <img src="<?php echo $thumbnail_url; ?>"
@@ -86,7 +120,12 @@ get_header();
                             <h5 class="card-title">
                                 <span class="line-clamp-3"><?php the_title(); ?></span>
                             </h5>
-                            <span class="badge bg-secondary">地元活性化サークル</span>
+                            <?php
+                            // サークル名を表示する
+                            if ( isset( get_the_category()[1]->name ) ):
+                            ?>
+                            <span class="badge bg-secondary"><?php echo get_the_category()[1]->name; ?></span>
+                            <?php endif; ?>
                             <p class="card-text"><small class="text-muted"><?php echo get_the_date(); ?></small></p>
                         </div>
                     </div>
