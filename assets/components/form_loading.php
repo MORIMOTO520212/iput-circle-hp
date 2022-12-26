8a49984d0b0992fdfdb7edf4823984f903f29090
YumaMorimoto <kanten520212@gmail.com>
2022-12-26 20:38:42 +0900

    add form loading component

---
+++ assets/components/form_loading.php
@@ -0,0 +1,49 @@
+<?php
+/**
+ * フォーム送信時にロードアニメーションを付ける。
+ * Usage:
+ * classにform-loadingを付与する。
+ * <form class="form-loading">...</form>
+ */
+
+function form_loading() {
+?>
+
+<!-- form loading animation -->
+<style>
+    div.spinner {
+        position: absolute;
+        top: 30vh;
+        left: calc(50% - 17px);
+        z-index: 500;
+    }
+    div.mask {
+        position: absolute;
+        top: 0;
+        left: 0;
+        width: 100%;
+        height: 100vh;
+        background: #ffffff96;
+        z-index: 450;
+    }
+</style>
+<div class="loading d-none">
+    <div class="spinner spinner-border text-primary" role="status">
+        <span class="visually-hidden">Loading...</span>
+    </div>
+    <div class="mask"></div>
+</div>
+
+<script>
+    var form = document.querySelector('.form-loading');
+    form.addEventListener('submit', () => {
+        console.log('form check validate');
+        if (form.checkValidity()) {
+            document.querySelector('.loading').classList.remove('d-none');
+        }
+    });
+</script>
+
+<?php
+}
+?>
