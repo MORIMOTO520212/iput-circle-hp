c5abb7363fd66baf2c9f8b5695e92f12e521f360
Yuma Morimoto <kanten520212@gmail.com>
2025-04-29 17:31:39 +0900

    add: get_discord_integrated_users

--- routes.php
+++ routes.php
@@ -30,3 +30,48 @@ function register_get_list_circle()
   );
 }
 add_action('rest_api_init', 'register_get_list_circle');
+
+
+function get_discord_integrated_users()
+{
+  $users = [];
+  $args = array(
+    'meta_query' => array(
+      array(
+        'key'     => 'discord_user_id',
+        'compare' => 'EXISTS',
+      ),
+    ),
+  );
+  $user_query = new WP_User_Query($args);
+
+  if (!empty($user_query->get_results())) {
+    foreach ($user_query->get_results() as $user) {
+      $first_name = get_user_option('first_name', $user->data->ID);
+      $last_name = get_user_option('last_name', $user->data->ID);
+
+      $users[] = [
+        'user_id' => $user->data->ID,
+        'discord_user_id' => get_user_meta($user->data->ID, 'discord_user_id', true),
+        'email' => $user->data->user_email,
+        'user_name' => $user->data->display_name,
+        'user_display_name' => "{$last_name} {$first_name}",
+      ];
+    }
+  }
+  return rest_ensure_response($users);
+}
+
+
+function register_get_discord_integrated_users()
+{
+  register_rest_route(
+    'custom/v2',
+    '/discord/integrated/users',
+    array(
+      'methods' => 'GET',
+      'callback' => 'get_discord_integrated_users'
+    )
+  );
+}
+add_action('rest_api_init', 'register_get_discord_integrated_users');
